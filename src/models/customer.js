b355c298738c91472d10eec1d44aa541fde76b33
Reza Panahi <rezabumanpanahi@gmail.com>
2026-01-14 08:26:38 +0430

    model fixed

--- src/models/customer.js
+++ src/models/customer.js
@@ -3,23 +3,8 @@ const {DataTypes}=require("sequelize");
 
 const Customer=sequelize.define("Customer",{
     id:{type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
-    name:{type:DataTypes.STRING, allowNull:false, validate:{
-        notEmpty:true,
-        is:{
-            args:[/^[a-zA-Z ]+$/],
-            msg:"name should be letters and space",
-        }
-    }},
-    phone:{type:DataTypes.STRING, allowNull:false, validate:{
-        isNumeric:{
-            args:true,
-            msg:"phone should be numbers only",
-        }, 
-        len:{
-            args:[10,10],
-            msg:"phone should be 10 digits",
-        }
-    }},
+    name:{type:DataTypes.STRING, allowNull:false},
+    phone:{type:DataTypes.STRING, allowNull:false},
 },{
     timestamps:true,
     indexes:[
--- src/models/product.js
+++ src/models/product.js
@@ -3,18 +3,8 @@ const {DataTypes}=require("sequelize");
 
 const Product=sequelize.define("Product",{
     id:{type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true},
-    name:{type:DataTypes.STRING, allowNull:false, unique:true, validate:{
-        notEmpty:{
-            args:true,
-            msg:"name cannot be empty",
-        }
-    }},
-    price:{type:DataTypes.DECIMAL(10,2), allowNull:false, validate:{
-        min:{
-            args:[0],
-            msg:"price can not be negative",
-        },
-    }},
+    name:{type:DataTypes.STRING, allowNull:false, unique:true},
+    price:{type:DataTypes.DECIMAL(10,2), allowNull:false},
 },{
     timestamps:true,
     indexes:[
